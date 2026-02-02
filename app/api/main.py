981ce491240b129543a0abb8f833d0bd76a145c6
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2026-02-02 20:44:19 -0300

    fix: backend prefix

--- app/api/main.py
+++ app/api/main.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from app.api.routes import loja, jogador, login, torneio, tipoJogador, ranking, estoque, credito
 from app.core.config import settings
 
-api_router = APIRouter(prefix={settings.API_PREFIX})
+api_router = APIRouter(prefix=settings.API_PREFIX)
 
 api_router.include_router(jogador.router)
 api_router.include_router(loja.router)
