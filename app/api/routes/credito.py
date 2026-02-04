af4772d2eec56eafb3fc8ff8035ce17e9ba820d0
AndreFelipeAlmeida <andre.neves@ccc.ufcg.edu.br>
2026-02-04 19:22:49 -0300

    feat: Get Credits by Player

--- app/api/routes/credito.py
+++ app/api/routes/credito.py
@@ -3,9 +3,9 @@ from sqlmodel import select
 from app.core.db import SessionDep
 from app.models import Credito, Loja
 from typing import List, Annotated
-from app.dependencies import retornar_loja_atual
+from app.dependencies import retornar_loja_atual, retornar_jogador_atual
 from app.core.security import TokenData
-from app.schemas.Credito import CreditoCreate, CreditoUpdate, CreditoAdd
+from app.schemas.Credito import CreditoCreate, CreditoUpdate, CreditoAdd, CreditoJogador
 
 router = APIRouter(
     prefix="/creditos",
@@ -21,6 +21,23 @@ def create_credito(credito_create: CreditoCreate, session: SessionDep, loja: Ann
     session.commit()
     session.refresh(credito)
     return credito
+
+
+@router.get("/jogador", response_model=List[CreditoJogador])
+def get_creditos_by_jogador(session: SessionDep, jogador: Annotated[TokenData, Depends(retornar_jogador_atual)]):
+    creditos = session.exec(select(Credito, Loja)
+                            .join(Loja, Loja.id == Credito.loja_id)
+                            .where(Credito.jogador_id == loja.id)).all()
+
+    creditos_formatados = []
+
+    for credito, loja in creditos:
+        credito_data = credito.model_dump()
+        credito_data["nome_loja"] = loja.nome
+        credito_data["endereco"] = loja.endereco
+        creditos_formatados.append(credito_data)
+
+    return creditos_formatados
 
 
 @router.get("/", response_model=List[Credito])
--- app/schemas/Credito.py
+++ app/schemas/Credito.py
@@ -1,4 +1,5 @@
 from sqlmodel import SQLModel
+from app.models import Credito
 
 
 class CreditoCreate(SQLModel):
@@ -12,3 +13,8 @@ class CreditoUpdate(SQLModel):
 
 class CreditoAdd(SQLModel):
     novos_creditos: float
+
+
+class CreditoJogador(Credito):
+    nome_loja: str
+    endereco: str
